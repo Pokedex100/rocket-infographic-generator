ec23482dbc571603eda0e356860fb84a15a16b63
yashrajbharticybtekk <y.bharti@cybtekk.com>
2023-12-30 22:43:55 +0530

    code changes

--- script.js
+++ script.js
@@ -22,13 +22,19 @@ const buildData = (data) => {
   let chartNodes = document.querySelector(".chart-container").childNodes;
   let headline = document.querySelector(".headline");
   let typeIcon = document.querySelectorAll(".grunt-type img.type");
+  let grunts = document.querySelectorAll(".grunt");
   let i = 0;
   for (let datum of data.reverse()) {
     // CHARACTER
     console.log(datum.character.name);
     let type = datum.character.name.split("_")[1].toLowerCase();
+    let grunt = datum.character.name.split("_")[3].toLowerCase();
+
     typeIcon[i].src = `./icons/${type}.svg`;
     typeIcon[i].classList.add(type);
+
+    grunts[i].src = `./icons/grunts/${grunt}.png`;
+    grunts[i].classList.add(grunt);
     // REWARDED POKEMON
     for (let reward of datum.rewards)
       console.log(reward.pokemon.name, !!reward.shinies);
