8e33ba847246d67d730a31e05723e39f9d8435c4
yashrajbharticybtekk <y.bharti@cybtekk.com>
2023-12-31 11:42:28 +0530

    code changes

--- script.js
+++ script.js
@@ -12,43 +12,111 @@ async function raidsFetch(base, endpoint, method, message) {
         return res.json();
       } else throw new Error("Not 2xx response", { cause: res });
     })
-    .then((data) => buildData(data.characters))
+    .then((data) => (cloning(data.characters), buildData(data.characters)))
     .catch((e) => console.log(e));
 }
 
 raidsFetch(BASEURL, "api/characters?hours=24", "GET");
 
+const cloning = (data) => {
+  for (let iteration of data) {
+    let gruntName = iteration.character.name.split("_")[2];
+    if (gruntName === "ARLO" || gruntName === "CLIFF" || gruntName === "SIERRA")
+      continue;
+    let temp = document.getElementsByTagName("template")[0];
+    let clone = temp.content.cloneNode(true);
+    document.querySelector(".chart-container").appendChild(clone);
+  }
+};
+
 const buildData = (data) => {
   let chartNodes = document.querySelector(".chart-container").childNodes;
+  let gruntNodes = document.querySelectorAll(".grunt-container");
   let headline = document.querySelector(".headline");
   let typeIcon = document.querySelectorAll(".grunt-type img.type");
   let grunts = document.querySelectorAll(".grunt");
   let i = 0;
   for (let datum of data) {
     // CHARACTER
-    console.log(datum.character.name);
-    let type = datum.character.name.split("_")[1].toLowerCase();
+    let gruntName = datum.character.name.split("_")[2];
+    if (gruntName === "ARLO" || gruntName === "CLIFF" || gruntName === "SIERRA")
+      continue;
 
+    let type = datum.character.name.split("_")[1].toLowerCase();
     let grunt =
       datum.character.name.split("_")[3]?.toLowerCase() ??
       datum.character.name.split("_")[2]?.toLowerCase();
-    if (type === "grunt") {
-      typeIcon[i].src = `./icons/ace.png`;
-      typeIcon[i].classList.add("ace");
-    } else {
-      typeIcon[i].src = `./icons/${type}.svg`;
-      typeIcon[i].classList.add(type);
+
+    switch (type) {
+      case "grunt": {
+        typeIcon[i].src = `./icons/ace.png`;
+        typeIcon[i].classList.add("ace");
+        break;
+      }
+      case "decoy": {
+        typeIcon[i].src = `./icons/ace.png`;
+        typeIcon[i].classList.add("ace");
+        break;
+      }
+      case "metal": {
+        typeIcon[i].src = `./icons/steel.svg`;
+        typeIcon[i].classList.add("steel");
+        break;
+      }
+      default: {
+        typeIcon[i].src = `./icons/${type}.svg`;
+        typeIcon[i].classList.add(type);
+      }
     }
+
     grunts[i].src = `./icons/grunts/${grunt}.png`;
     grunts[i].classList.add(grunt);
     // REWARDED POKEMON
+    let shinyMap = new Map();
     for (let reward of datum.rewards)
-      console.log(reward.pokemon.name, !!reward.shinies);
+      shinyMap.set(reward.pokemon.name, !!reward.shinies);
     // TEAM OF POKEMONS TO FIGHT WITH
-    for (let team of datum.team) console.log(team.pokemon.name, team.slot);
+    let stage1 = gruntNodes[i].querySelectorAll(".stage-1 span img");
+    let stage2 = gruntNodes[i].querySelectorAll(".stage-2 span img");
+    let stage3 = gruntNodes[i].querySelectorAll(".stage-3 span img");
+    let a = 0;
+    let b = 0;
+    let c = 0;
+    for (let team of datum.team)
+      switch (team.slot) {
+        case 0: {
+          stage1[
+            a
+          ].src = `https://img.pokemondb.net/sprites/home/normal/${team.pokemon.name.toLowerCase()}.png`;
+          shinyMap.get(team.pokemon.name) && stage1[a].classList.add("shiny");
+          !shinyMap.has(team.pokemon.name) &&
+            stage1[a]?.closest("div").classList.remove("encounterable");
+          a++;
+          break;
+        }
+        case 1: {
+          stage2[
+            b
+          ].src = `https://img.pokemondb.net/sprites/home/normal/${team.pokemon.name.toLowerCase()}.png`;
+          shinyMap.get(team.pokemon.name) && stage2[b].classList.add("shiny");
+          !shinyMap.has(team.pokemon.name) &&
+            stage2[b]?.closest("div").classList.remove("encounterable");
+          b++;
+          break;
+        }
+        case 2: {
+          stage3[
+            c
+          ].src = `https://img.pokemondb.net/sprites/home/normal/${team.pokemon.name.toLowerCase()}.png`;
+          shinyMap.get(team.pokemon.name) && stage3[c].classList.add("shiny");
+          !shinyMap.has(team.pokemon.name) &&
+            stage3[c]?.closest("div").classList.remove("encounterable");
+          c++;
+          break;
+        }
+      }
     // SEPARATION
     console.warn("AAAAAAAAAAA");
     i++;
-    if (i === 2) break;
   }
 };
